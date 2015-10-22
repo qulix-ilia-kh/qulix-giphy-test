69eb4cc0e08d5ddb5756e45767c76ce8a080e323
Ilia Khatkevich <IKhatkevich@qulix.com>
2015-10-22 15:28:48 +0300

    cleanup default data and add rxjava and recycler view dependencies

--- GiphyTestApp/app/src/main/java/qulix/com/giphytestapp/MainActivity.java
+++ GiphyTestApp/app/src/main/java/qulix/com/giphytestapp/MainActivity.java
@@ -1,37 +1,19 @@
 package qulix.com.giphytestapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView mRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qulix_giphy_test);
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.gifs_list);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_qulix_giphy_test, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
